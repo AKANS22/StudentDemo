df0b1c559ed7001bb46bdaab23c05a866cb8ed8f
Akans Udoka <Akansudoka7@gmail.com>
2023-06-19 17:16:56 +0100

    created a new project called models

---
+++ StudentEnrollmentModels/BaseModel.cs
@@ -0,0 +1,22 @@
+﻿namespace StudentEnrollmentModels
+{
+    public abstract class BaseModel
+    {
+        int Id { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string createdBy { get; set; }
+        public DateTime? UpdatedDate { get;}
+        public string updatedBy { get; set; }
+
+    }
+
+    public class Student: BaseModel 
+    {
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string StudentId { get; set; } = string.Empty;
+
+    }
+}
